77752b7016fca82321b2f0e950407094c1a2da7f
ophirmil12 <ophirmil12@gmail.com>
2026-01-14 15:47:50 +0200

    added p9 todos, fixes for p6

---
+++ p9_make_results_excel.py
@@ -0,0 +1,23 @@
+# Prepare an Excel file with a summary of the results
+
+
+
+# TODO
+#  We want the file to be as followed:
+#  For each sheet, named {cancer type}:
+#       Have a table with columns:
+#           Pathway ID
+#           Pathway name (description from metadata)
+#           Wasserstein distance
+#           Delta of expectations
+#           Q-value
+#           Coverage
+#           More statistics...
+
+
+
+
+
+
+
+
