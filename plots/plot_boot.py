ce654e7fb131d5fc60ecf60e6925b13ea474a23d
lotemsenderov <lotem.senderov@mail.huji.ac.il>
2026-01-22 11:34:33 +0200

    fixed coloring

--- plots/plot_boot.py
+++ plots/plot_boot.py
@@ -5,5 +5,5 @@ def boot_plot_folder():
     back_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     sys.path.append(back_dir)
 
-    from definitions import set_paper_palette, COLOR_MAP
+    from definitions import set_paper_palette
     set_paper_palette()
